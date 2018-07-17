d55540587e8502cc70feb17a39a80affcbd41c7d
dachunchun <524112857@qq.com>
2018-07-17 16:16:20 +0800

    up 简单实用express脚手架

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => {
+    res.render('index', {title: '大春春'})
+})
 
-module.exports = router;
+module.exports = router
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var router = express.Router()
+
+const users = []
+
+/* GET home page. */
+router.route('/')
+    .get((req, res, next) => {
+        console.log(users)
+        res.json(users)
+    })
+    .post((req, res) => {
+        const user = req.query
+        users.push(user)
+        res.json(user)
+    })
+
+module.exports = router
