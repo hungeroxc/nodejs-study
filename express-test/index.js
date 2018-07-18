8416f757179d167de5051c02148855e0c6cc27ab
dachunchun <524112857@qq.com>
2018-07-18 11:34:21 +0800

    up express中间件间的属性传递

--- express-test/index.js
+++ express-test/index.js
@@ -2,13 +2,15 @@ const express = require('express')
 
 const app = express()
 
-// 中间件的使用
+// 中间件的使用, 声明周期由上往下传递
 app.use((req, res, next) => {
-    console.log(321)
+    // 设置的属性可以被下一个中间件接收到
+    req.duang = 1
     next()
 })
 
 app.use((req, res) => {
+    console.log(req.duang)
     res.end('123')
 })
 
