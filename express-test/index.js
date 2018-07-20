6370913d18d02ae445091d180284b2b41e58c3bc
dachunchun <524112857@qq.com>
2018-07-20 15:40:30 +0800

    up 中间件鉴权测试

--- express-test/index.js
+++ express-test/index.js
@@ -2,6 +2,18 @@ const express = require('express')
 
 const app = express()
 
+// 中间件鉴权测试
+function auth(req, res, next) {
+    console.log(req.query)
+    if(req.query.username === 'oxc') {
+        next()
+    } else {
+        res.end('please go away')
+    }
+}
+
+app.use(auth)
+
 // 中间件的使用, 声明周期由上往下传递
 app.use((req, res, next) => {
     // 设置的属性可以被下一个中间件接收到
