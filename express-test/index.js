e9aa0bf51a76851313c1d5dc2a54f168179d8499
oxc <oxc@oxc.com>
2018-07-25 07:51:03 +0800

    up 通过路径和请求方式进行中间件流程控制

--- express-test/index.js
+++ express-test/index.js
@@ -26,7 +26,27 @@ function mw3(req, res, next) {
     res.end('done')
 }
 
-app.use(mw1(), [mw2, mw3])
+// app.use(mw1(), [mw2, mw3])
+
+// 路径控制
+/**
+ * @description 用于请求控制(网关)
+ * 
+ * 1、根目录打印mw1和mw3
+ * 2、加上article打印mw1、mw2、mw3
+ */
+// app.use('/', mw1())
+// app.use('/article', mw2)
+// app.use(mw3)
+
+/**
+ * @description 加上请求方式控制
+ * 
+ * article只针对get请求响应，user只针对post请求响应，跟路由针对所有请求响应
+ */
+app.use('/', mw1())
+app.get('/article', mw2)
+app.post('/user', mw3)
 
 // 错误处理
 app.use((err, req, res, next) => {
