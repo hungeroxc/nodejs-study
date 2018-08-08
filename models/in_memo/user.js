acd5482e9f5c96ccee1db6bab9f526cb4d47369c
oxc <oxc@oxc.com>
2018-08-08 08:05:13 +0800

    up mvc之简易存储和获取结构

--- models/in_memo/user.js
+++ models/in_memo/user.js
@@ -1,13 +1,33 @@
+const users = []
+
 class User {
     constructor(firstname, lastname, age) {
         this.firstname = firstname
         this.age = age
         this.lastname = lastname
     }
-
-    getName() {
+    get name() {
         return `${this.firstname} ${this.lastname}`
     }
+
+    static insert(firstname, lastname, age) {
+        const u = new User(firstname, lastname, age)
+        User.users.push(u)
+        return u
+    }
+
+    static getOneByName(firstname, lastname) {
+        return User.users.find(u => u.firstname === firstname && u.lastname === lastname)
+    }
+
+    static list(query) {
+        return User.users
+    }
+
+    static get ['users'] () {
+        return users
+    }
 }
+
 
 module.exports = User
